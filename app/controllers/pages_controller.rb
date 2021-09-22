43376c474be7087e339b688e453b2e3bee096f9e
Anne Bonfante <bonfante.anne@gmail.com>
2021-09-22 16:03:08 +0200

    creating an interface

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,16 @@
 class PagesController < ApplicationController
   def calc
-    # render :text => eval(params[:id])
+    if operator == "+"
+      result = first_term + second_term
+    elsif operator == "-"
+      result = first_term - second_term
+    elsif operator == "*"
+      result = first_term * second_term
+    elsif operator == "/"
+      result = first_term / second_term
+    else
+      puts "wrong operator"
+    end
+    return result
   end
 end
