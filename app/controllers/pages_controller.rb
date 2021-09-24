cf9b9652f3ada6bbfb18e56a314bd187992b7ff3
Anne Bonfante <bonfante.anne@gmail.com>
2021-09-24 16:14:45 +0200

    doesn't want the erb balises

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,16 +1,20 @@
 class PagesController < ApplicationController
+
   def calc
-    if operator == "+"
-      result = first_term + second_term
-    elsif operator == "-"
-      result = first_term - second_term
-    elsif operator == "*"
-      result = first_term * second_term
-    elsif operator == "/"
-      result = first_term / second_term
-    else
-      puts "wrong operator"
-    end
-    return result
+    # @first_term = first_term
+    # @operator = operator
+    # @second_term = second_term
+    # if operator == "+"
+    #   result = first_term + second_term
+    # elsif operator == "-"
+    #   result = first_term - second_term
+    # elsif operator == "*"
+    #   result = first_term * second_term
+    # elsif operator == "/"
+    #   result = first_term / second_term
+    # else
+    #   puts "wrong operator"
+    # end
+    # return result
   end
 end
